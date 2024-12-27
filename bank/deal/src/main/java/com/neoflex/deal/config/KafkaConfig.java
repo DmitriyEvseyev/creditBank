e90d540792f777959efb2594fb43638210d7688b
Dmitriy <yevseyevdmitriy@yandex.ru>
2024-12-27 22:21:21 +0400

    kafka edit 3.0

--- bank/deal/src/main/java/com/neoflex/deal/config/KafkaConfig.java
+++ bank/deal/src/main/java/com/neoflex/deal/config/KafkaConfig.java
@@ -15,26 +15,7 @@ import java.util.List;
 @Configuration
 @Slf4j
 public class KafkaConfig {
-//    @Value("${spring.kafka.topics}")
-//    private List<String> topicsList;
-//
-//    @PostConstruct
-//    public void init() {
-//        log.info("Initialized topics list: {}", topicsList);
-//    }
-//
-//    @Bean
-//    KafkaAdmin.NewTopics createTopics() {
-//        List<NewTopic> topics = new ArrayList<>();
-//        for (String name : topicsList) {
-//            topics.add(TopicBuilder.name(name)
-//                    .partitions(3)
-//                    .replicas(1)
-//                    .build());
-//            log.info("Creating topic: {}", name);
-//        }
-//        return new KafkaAdmin.NewTopics(topics.toArray(NewTopic[]::new));
-//    }
+
     @Bean
     public NewTopic finishRegistrationtopic() {
         return TopicBuilder.name("finish_registration")
