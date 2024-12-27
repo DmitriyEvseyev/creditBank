c76de6085895be67dc0dc0d4b015b96d5691b89e
Dmitriy <yevseyevdmitriy@yandex.ru>
2024-12-27 22:13:05 +0400

    kafka edit 2.0

--- bank/deal/src/main/java/com/neoflex/deal/config/KafkaConfig.java
+++ bank/deal/src/main/java/com/neoflex/deal/config/KafkaConfig.java
@@ -15,71 +15,71 @@ import java.util.List;
 @Configuration
 @Slf4j
 public class KafkaConfig {
-    @Value("${spring.kafka.topics}")
-    private List<String> topicsList;
-
-    @PostConstruct
-    public void init() {
-        log.info("Initialized topics list: {}", topicsList);
+//    @Value("${spring.kafka.topics}")
+//    private List<String> topicsList;
+//
+//    @PostConstruct
+//    public void init() {
+//        log.info("Initialized topics list: {}", topicsList);
+//    }
+//
+//    @Bean
+//    KafkaAdmin.NewTopics createTopics() {
+//        List<NewTopic> topics = new ArrayList<>();
+//        for (String name : topicsList) {
+//            topics.add(TopicBuilder.name(name)
+//                    .partitions(3)
+//                    .replicas(1)
+//                    .build());
+//            log.info("Creating topic: {}", name);
+//        }
+//        return new KafkaAdmin.NewTopics(topics.toArray(NewTopic[]::new));
+//    }
+    @Bean
+    public NewTopic finishRegistrationtopic() {
+        return TopicBuilder.name("finish_registration")
+                .partitions(3)
+                .replicas(1)
+                .build();
     }
 
     @Bean
-    KafkaAdmin.NewTopics createTopics() {
-        List<NewTopic> topics = new ArrayList<>();
-        for (String name : topicsList) {
-            topics.add(TopicBuilder.name(name)
-                    .partitions(3)
-                    .replicas(1)
-                    .build());
-            log.info("Creating topic: {}", name);
-        }
-        return new KafkaAdmin.NewTopics(topics.toArray(NewTopic[]::new));
+    public NewTopic createDocumentsTopic() {
+        return TopicBuilder.name("create_documents")
+                .partitions(3)
+                .replicas(1)
+                .build();
     }
-//    @Bean
-//    public NewTopic finishRegistrationtopic() {
-//        return TopicBuilder.name("finish_registration")
-//                .partitions(3)
-//                .replicas(1)
-//                .build();
-//    }
-//
-//    @Bean
-//    public NewTopic createDocumentsTopic() {
-//        return TopicBuilder.name("create_documents")
-//                .partitions(3)
-//                .replicas(1)
-//                .build();
-//    }
-//
-//    @Bean
-//    public NewTopic sendDocumentsTopic() {
-//        return TopicBuilder.name("send_documents")
-//                .partitions(3)
-//                .replicas(1)
-//                .build();
-//    }
-//
-//    @Bean
-//    public NewTopic sendSesTopic() {
-//        return TopicBuilder.name("send_ses")
-//                .partitions(3)
-//                .replicas(1)
-//                .build();
-//    }
-//
-//    @Bean
-//    public NewTopic creditIssuedTopic() {
-//        return TopicBuilder.name("credit_issued")
-//                .partitions(3)
-//                .replicas(1)
-//                .build();
-//    }
-//
-//    @Bean
-//    public NewTopic statementDeniedTopic() {
-//        return TopicBuilder.name("statement_denied")
-//                .partitions(3)
-//                .replicas(1)
-//                .build();
-//    }
+
+    @Bean
+    public NewTopic sendDocumentsTopic() {
+        return TopicBuilder.name("send_documents")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic sendSesTopic() {
+        return TopicBuilder.name("send_ses")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic creditIssuedTopic() {
+        return TopicBuilder.name("credit_issued")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic statementDeniedTopic() {
+        return TopicBuilder.name("statement_denied")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 }
